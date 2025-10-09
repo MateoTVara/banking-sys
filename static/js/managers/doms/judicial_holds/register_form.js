c742685737395a18263f201a487522f104ae379f
MateoTVara <146038574+MateoTVara@users.noreply.github.com>
2025-10-09 15:42:54 -0500

    refactor: modulariza lógica JS del formulario de embargo judicial
    - Extrae el script de control de campo "Monto" a register_form.js como clase modular
    - Elimina el script embebido en register_form.html

--- static/js/managers/doms/judicial_holds/register_form.js
+++ static/js/managers/doms/judicial_holds/register_form.js
@@ -1,7 +1,33 @@
+import { DomManager } from "../../index.js";
+
 export class JudicialHoldManagerRegisterFormManager {
   constructor() {}
 
+  static #setupDomElements() {
+    return DomManager.selectDomElements({
+      holdTypeSelect: '#hold_type',
+      amountGroup: '#amount-group',
+      holdAmount: '#hold_amount',
+    });
+  }
+
+  static #initHoldTypeSelectListeners(elements) {
+    elements.holdTypeSelect.addEventListener('change', () => {
+      if (elements.holdTypeSelect.value === 'partial') {
+        elements.amountGroup.style.display = '';
+        elements.holdAmount.required = true;
+      } else {
+        elements.amountGroup.style.display = 'none';
+        elements.holdAmount.required = false;
+        elements.holdAmount.value = '';
+      }
+    });
+  }
+
   static initOnDomLoad(){
     document.querySelector('main').classList.add('judicial-hold-register-form-body');
+    const elements = this.#setupDomElements();
+    this.#initHoldTypeSelectListeners(elements);
+    elements.holdTypeSelect.dispatchEvent(new Event('change'));
   }
 }
